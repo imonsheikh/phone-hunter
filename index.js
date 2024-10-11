b822233d2b987016938361a8341e224329e75b6b
imon-sheikh <mdimonhossain2017@gmail.com>
2024-10-11 20:21:59 +0600

    data load by ternary operator

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const loadAllPhones = async(status) =>{
+const loadAllPhones = async(status, brandName) =>{
+    console.log("Hello I come from handle handleSearch() function by load by loadAllPhones() function");
+    
     console.log("wow 3 second gone")
 document.getElementById('spinner').style.display = 'none'
 
 //async await 
-const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`)
+const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${brandName? brandName : "iphone"}`)
 const data = await response.json()
 // console.log(data.data)
-displayAllPhones(data.data.slice(0,6))
+// displayAllPhones(data.data.slice(0,6))
 
 // console.log(status);
-if(status){
+if(status){ //when true
 displayAllPhones(data.data)
 
-}else{
+}else{ //when false
 displayAllPhones(data.data.slice(0,6))
 
 }
@@ -41,13 +43,18 @@ const handleSearch = () =>{
 // console.log("hello");
 document.getElementById('spinner').style.display = 'block'
 
+const searchText = document.getElementById('search-box').value
+
 
 setTimeout(function () {
 
-    loadAllPhones()
+    loadAllPhones(false, searchText) //when false
     console.log("hello from handle search");
     
 
-},3000)
+},2000)
 
 }
+
+
+loadAllPhones(false, "iphone")
